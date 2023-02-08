1ee69a8190c06e8a1347dc2605f6f49fe1a6004c
McYeremia <117885019+McYeremia@users.noreply.github.com>
2023-02-08 18:27:42 +0700

    Add files via upload

---
+++ 4_D_71220953.py
@@ -0,0 +1,25 @@
+print('---------- Nilai ke 1 ---------')
+a1=int(input('Nilai Harian:'))
+a2=int(input('Nilai UTS: '))
+a3=int(input('Nilai UAS: '))
+
+print('---------- Nilai ke 2 ---------')
+b1=int(input('Nilai Harian:'))
+b2=int(input('Nilai UTS: '))
+b3=int(input('Nilai UAS: '))
+
+tugas=((a1+b1)*(30/100))
+uts=((a2+b2)*(30/100))
+uas=((a3+b3)*(40/100))
+hasil=round((tugas+uts+uas)/2,0)
+
+print('--------- Total Nilai -------')
+print('Total Nilai yang didapat :', hasil)
+if hasil >= 85:
+    print('Total nilai dalam huruf: A')
+elif hasil >= 70:
+    print('Total nilai dalam huruf: B')
+elif hasil >= 45:
+    print('Total nilai dalam huruf: C')
+elif hasil >= 30:
+    print('Total nilai dalam huruf: D')
